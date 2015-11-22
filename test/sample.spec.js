9c7a07c88cf81515f2ead0dd388632ef6894c946
Zarema Khalilova <zarema.khalilova@gmail.com>
2015-11-22 16:53:02 +0300

    Created index.js and sample.js in /src and created test for sample.

---
+++ test/sample.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import expect from 'expect';
+import sample from '../src/sample';
+
+describe('Sample', ()=>{
+  it('return true', ()=>{
+    expect(sample()).toBe(true);
+  })
+});
