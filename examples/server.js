d428118b820fa685a5b1b89756c5632bbcb369cf
Zarema Khalilova <zarema.khalilova@gmail.com>
2015-12-10 18:57:04 +0300

    Change data loading. Added ajax load.

--- examples/server.js
+++ examples/server.js
@@ -1,6 +1,9 @@
-var argv = require("yargs").argv;
+"use strict";
 
-if(!argv.example){
+var argv = require("yargs").argv;
+var wordsChecker = require("./../lib/words-checker").default;
+
+if (!argv.example) {
   throw new Error("You don't choose example. Please pass 'npm start -- --example=examplename'.");
 }
 var example = argv.example;
@@ -18,6 +21,43 @@ var port = 3000;
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
+
+app.get("/items-search", function(req, res) {
+  const q = req.query.q || "";
+  const searchIn = req.query["search_in"] ? req.query["search_in"].split(",") : ["first_name", "last_name", "domain"];
+  let items = require("./data.json").items;
+
+  if (q.length) {
+    let words = wordsChecker.getConditionalWords(q);
+
+    items = items.filter(item => {
+      for (let field of searchIn) {
+        if(item[field] == undefined){
+          return false;
+        }
+        const value = item[field].toLowerCase();
+        for (let word of words) {
+          if (~value.search(word)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+  }
+
+  items = items.map(item => {
+    return {
+      id: item.uid,
+      title: [item["first_name"], item["last_name"]].join(" "),
+      image: item["photo_50"]
+    };
+  });
+
+  res.json({
+    items
+  });
+});
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, example, "index.html"));
